ce6687d7edac4edd05195c39bcef565c3f66ed78
Vlad-Pischaeff <pischaeff@gmail.com>
2023-01-26 14:24:17 +0500

    update user profile

--- dashboard/src/pages/Dashboard/UserProfile.tsx
+++ dashboard/src/pages/Dashboard/UserProfile.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './UserProfile.module.sass';
 
 export const UserProfile = () => {
     return (
-        <div>
-            User Profile...
+        <div className={s.ProfileContainer}>
+            <p>User Profile...</p>
         </div>
     );
 };
