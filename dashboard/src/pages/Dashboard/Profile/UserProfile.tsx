ff3652734336e0ead25a8522c501d11d91379232
Vlad-Pischaeff <pischaeff@gmail.com>
2023-01-28 21:17:17 +0500

    update user profile

--- dashboard/src/pages/Dashboard/Profile/UserProfile.tsx
+++ dashboard/src/pages/Dashboard/Profile/UserProfile.tsx
@@ -4,8 +4,8 @@ import { useGetUserQuery } from 'store/api/usersApi';
 import { selectCurrentUser } from 'store/slices/auth';
 import { setServicesModal, selectUIServicesModal, eModal } from "store/slices/ui";
 import { UserProfileImage } from './UserProfileImage';
+import { UserProfileWebsites } from './UserProfileWebsites';
 import { UserProfileAddSiteForm } from './UserProfileAddSiteForm';
-import * as ICON from 'assets/icons';
 import s from './UserProfile.module.sass';
 
 export const UserProfile = () => {
@@ -47,34 +47,8 @@ export const UserProfile = () => {
                             <p className={s.ItemValue}>{data.email}</p>
                         </div>
                         <hr/>
-                        <div className={s.Item}>
-                            <div className={s.ItemTitle}>Web-sites: </div>
-                            <div className={s.ItemContainer} role="listbox">
-                                { data.websites.length === 0
-                                    ? <div className={s.ItemNoValue}>No managed sites...</div>
-                                    : <>
-                                        { data.websites.map(item => {
-                                            return <div
-                                                        role="listitem"
-                                                        key={item.hash}
-                                                        className={s.PropertyContainer}
-                                                    >
-                                                        <p className={s.PropertySite}>{item.site}</p>
-                                                        {/* <p className={s.ItemTitle}>hash:</p>
-                                                        <p className={s.PropertyHash}>{item.hash}</p> */}
-                                                        <div className={s.PropertyIcon}>
-                                                            <ICON.EditIcon />
-                                                        </div>
-                                                        <div className={s.PropertyIcon}>
-                                                            <ICON.TrashIcon />
-                                                        </div>
-                                                    </div>
-                                            })
-                                        }
-                                    </>
-                                }
-                            </div>
-                        </div>
+
+                        <UserProfileWebsites />
 
                         <input
                             type="button"
@@ -83,8 +57,6 @@ export const UserProfile = () => {
                             onClick={openModalAddSite}
                         />
                     </div>
-                        {/* <hr/> */}
-
                     </>
                 }
             </div>
---
+++ dashboard/src/pages/Dashboard/Profile/UserProfileWebsites.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+// eslint-disable-next-line
+import { useAppSelector, useAppDispatch } from 'store/hook';
+import { useGetUserQuery } from 'store/api/usersApi';
+import { selectCurrentUser } from 'store/slices/auth';
+// import { setServicesModal, selectUIServicesModal, eModal } from "store/slices/ui";
+// import { UserProfileImage } from './UserProfileImage';
+// import { UserProfileAddSiteForm } from './UserProfileAddSiteForm';
+import * as ICON from 'assets/icons';
+import s from './UserProfile.module.sass';
+
+export const UserProfileWebsites = () => {
+    const user = useAppSelector(selectCurrentUser);
+    const { data } = useGetUserQuery(user.id, { skip: !user.id });
+
+    return (
+        <div className={s.Item}>
+            <div className={s.ItemTitle}>Web-sites: </div>
+            <div className={s.ItemContainer} role="listbox">
+                { data && data.websites.length === 0
+                    ? <div className={s.ItemNoValue}>No managed sites...</div>
+                    : <>
+                        { data && data.websites.map(item => {
+                            return <div
+                                        role="listitem"
+                                        key={item.hash}
+                                        className={s.PropertyContainer}
+                                    >
+                                        <p className={s.PropertySite}>{item.site}</p>
+                                        {/* <p className={s.ItemTitle}>hash:</p>
+                                        <p className={s.PropertyHash}>{item.hash}</p> */}
+                                        <div className={s.PropertyIcon}>
+                                            <ICON.EditIcon />
+                                        </div>
+                                        <div className={s.PropertyIcon}>
+                                            <ICON.TrashIcon />
+                                        </div>
+                                    </div>
+                            })
+                        }
+                    </>
+                }
+            </div>
+        </div>
+    );
+};
